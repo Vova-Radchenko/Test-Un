2147a2e392e02c71a1090bb05fb0f04f2a54aae5
Вова Радченко <vettel070897@gmail.com>
2021-01-14 18:50:18 +0200

    first commit

---
+++ Laba_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter x : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y : ");
+            int y = int.Parse(Console.ReadLine());
+            double z = 0;
+            if (x < 0 && y > 10)
+            {
+                z = Math.Asinh(x) + 2;
+            }
+            else if (x > 0 && y <= 1)
+            {                
+                z = Math.Acosh(x) - y;                
+            }
+            else
+            {
+                Console.WriteLine("You enter wrong \"x\" and \"y\"!!!");
+            }
+            Console.WriteLine("z = " + z);
+        }
+
+
+    }
+}
